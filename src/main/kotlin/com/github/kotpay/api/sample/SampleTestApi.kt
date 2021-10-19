0b7e94cd1a1b15462b4d766c845644b4e0921471
leuvaarden <probablyviolin@gmail.com>
2021-10-19 10:16:01 +0300

    Init

--- src/main/kotlin/com/github/kotpay/api/sample/SampleTestApi.kt
+++ src/main/kotlin/com/github/kotpay/api/sample/SampleTestApi.kt
@@ -3,7 +3,7 @@ package com.github.kotpay.api.sample
 import com.github.kotpay.api.dto.Response
 
 
-interface SampleTestController {
+interface SampleTestApi {
     fun getHello(name: String): Response<String>
 
     fun getException(): Response<String>
